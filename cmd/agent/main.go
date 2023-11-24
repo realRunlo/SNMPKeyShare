e7a629b8af81e140e425e594e799b42643d9ac0e
Gonçalo Pereira <gteixeira.pereira@gmail.com>
2023-11-24 17:05:18 +0000

    agent config parser

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,31 +2,41 @@ package main
 
 import (
     "os"
+     "github.com/realRunlo/SNMPKeyShare/pkg/agent/parser"  
      log "github.com/sirupsen/logrus"
      km "github.com/realRunlo/SNMPKeyShare/pkg/agent/keyManagement" 
 )
 
 func init() {
-  // Log as JSON instead of the default ASCII formatter.
- // log.SetFormatter(&log.JSONFormatter{})
+
+    // Text format
     log.SetFormatter(&log.TextFormatter{
 		FullTimestamp: true,
 	})
-  // Output to stdout instead of the default stderr
-  // Can be any io.Writer, see below for File example
-  log.SetOutput(os.Stdout)
+    // Output to stdout instead of the default stderr
+    log.SetOutput(os.Stdout)
 
-  // Only log the warning severity or above.
-  log.SetLevel(log.TraceLevel)
+    // Only log the warning severity or above.
+    log.SetLevel(log.TraceLevel)
 }
 
 // 126 - 33 + 1 = 94 - range of visable ASCII chars
 const FM_SIZE int = 94
 
 func main() {
-    var master_key string = "abcdefghijklmn"
-    var fm_matrix [][]int32 = km.Generate_random_matrix(FM_SIZE)
-    var z = km.Generate_matrix(fm_matrix,master_key)
+     // Generate fm matrix on another thread
+    fm_channel := make(chan [][]int32)
+    go func() {
+	 fm_channel <- km.Generate_random_matrix(FM_SIZE)
+	 log.Info("FM matrix generated")
+    }()
+    // Parse config file
+    var confs parser.AgentConf
+    confs = parser.Parse_config_file("../../etc/agentConf.json")
+
+    var fm_matrix [][]int32 = <-fm_channel
+
+    var z = km.Generate_matrix(fm_matrix,confs.Master_key)
     var updated_z = km.Update_matrix(z)
     km.Keygen(fm_matrix,updated_z,1)
 }
--- pkg/agent/parser/parser.go
+++ pkg/agent/parser/parser.go
@@ -1,5 +1,32 @@
 package parser
 
+import (
+    "os"
+    "encoding/json"
+     log "github.com/sirupsen/logrus"
+)
 
+type AgentConf struct{
+    Port int 	//udp port
+    Master_key string // master key (required)
+    Update_interval int  //matrix update interval (seconds)
+    Expire_interval int //key expire interval 	(seconds)
+}
 
-//Todo parse agent config file
+func Parse_config_file(filename string) AgentConf{
+    
+    var confs AgentConf
+    
+    // Read the contents of the file
+    file_content, read_err := os.ReadFile(filename)
+    if read_err != nil {
+	log.Fatal(read_err)
+    }
+
+    parse_err := json.Unmarshal(file_content, &confs)
+    if parse_err != nil {
+	log.Fatal(parse_err)
+    }
+    log.Info("Agent configs loaded")
+    return confs
+}
